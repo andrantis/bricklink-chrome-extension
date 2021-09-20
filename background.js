d8dd9ccba15659d09bbfcfe95c683fb7811f2cf1
Andres Antista <170717+andrantis@users.noreply.github.com>
2021-09-19 20:56:00 -0700

    better matching

--- background.js
+++ background.js
@@ -7,7 +7,7 @@ chrome.runtime.onInstalled.addListener(function () {
 });
 
 chrome.contextMenus.onClicked.addListener(function (item, tab) {
-  const legoset = item.selectionText.replace(/[^0-9]/g, '');
+  const legoset = item.selectionText.match(/\d{5}/)[0];
   const url = `https://www.bricklink.com/catalogPOV.asp?itemType=S&itemNo=${legoset}&itemSeq=1&itemQty=1&breakType=M&itemCondition=N&incInstr=Y&incParts=Y`;
   chrome.tabs.create({ url: url, index: tab.index + 1 });
 });
